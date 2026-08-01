cdba45dc30393f7a1074039bf7e723aa9258fb6e
Arturo Underwood <arturo@ridian.io>
2026-08-01 15:09:12 -0600

    Convert to static HTML/CSS with no build step

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tailwindcss()],
-})
